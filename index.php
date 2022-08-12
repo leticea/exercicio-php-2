13850bb9d64475d10b2543916b977cf2037ddacc
leticea <nascimento.leticeah@gmail.com>
2022-08-12 14:33:59 -0300

    iniciando o projeto

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Exercícios Básicos</title>
+        
+        <style>
+
+            *{
+                margin: 0px;
+                padding: 0px;
+                color: indigo;
+                font-family: Arial;
+            }
+
+            div { 
+                border: 1px solid black;
+                margin: 10px;
+                padding: 10px;
+                width: 120px;
+                
+            }
+
+        </style>
+    </head>
+    <body>
+         <div>
+            <p><b><a href="http://localhost/exercicios-php-2/exercicios/exerc_21.php">Exercício 21</a></b></p><br>         
+            <p><b><a href="http://localhost/exercicios-php-2/exercicios/exerc_22.php">Exercício 22</a></b></p><br>         
+      
+        </div>    
+    </body>
+           
+</html>
+
+
+
+
+
